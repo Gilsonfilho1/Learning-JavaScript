c639d076a0cbcd870bf4ff06d44fc207c53748aa
Gilson filho <101582105+Gilsonfilho1@users.noreply.github.com>
2022-08-05 21:23:02 -0300

    Add files via upload

---
+++ EXERCICIOS-JS/Fundamentos/comentario.js
@@ -0,0 +1,10 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log("Legal 1");
+console.log("Legal 2"); // mais um exemplo de console.log!
+console.log("Legal 3");
+/*
+Esse é um
+comentário
+De varias
+linhas
+*/
